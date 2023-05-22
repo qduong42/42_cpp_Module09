5b82cc6c29d8373cda02d63ef2c18c0b7b985d4f
qduong42 <15814168+qduong42@users.noreply.github.com>
2023-05-22 12:20:40 +0200

    took out pre-print to individual function

--- ex02/include/PmergeMe.hpp
+++ ex02/include/PmergeMe.hpp
@@ -3,13 +3,14 @@
 
 #include <iostream>
 #include <vector>
-#include <list>
-#include <utility>
-#include <iomanip>
-#include <algorithm>
 #include <deque>
+//indirectly included in vector & deque
+#include <utility>
+#include <algorithm>
 
 #define SEC_TO_MS 1000000
+
+//HELPER FUNCTIONS: Not included as class functions
 
 int	jacobsthalNumber(int idx);
 
@@ -26,10 +27,12 @@ class PmergeMe
 	double getProcessorTime() const;
 	int binarySearch(const A&, const int&, const int, const int)const;
 	void output_print(const A&, const double&)const;
+	void pre_print_info(const int &, char**);
 
 	public:
+
 		PmergeMe();
-		PmergeMe(const int, char **);
+		PmergeMe(const int&, char **);
 		PmergeMe(PmergeMe const& obj);
 		PmergeMe& operator=(PmergeMe const& obj);
 		~PmergeMe();
@@ -45,21 +48,8 @@ class PmergeMe
 };
 
 template <typename A, typename B>
-void PmergeMe<A, B>::output_print(const A& container, const double &timeDifference)const
-{
-	if (typeid(container) == typeid(std::deque<typename A::value_type, typename A::allocator_type>))
-	{
-		std::cout << "Time to process a range of " << container.size()
-			  	  << " elements with std::deque : " 
-			  	  <<  timeDifference << " us" << std::endl;
-	}
-	else if (typeid(container) == typeid(std::vector<typename A::value_type, typename A::allocator_type>))
-	{
-		std::cout << "Time to process a range of " << container.size()
-		  		  << " elements with std::vector : " 
-		  		  <<  timeDifference << " us" << std::endl;
-	}
-}
+PmergeMe<A, B>::PmergeMe() :_straggler(-1)
+{}
 
 //custom mycomparison function to sort by second
 // struct myComparison
@@ -71,69 +61,69 @@ void PmergeMe<A, B>::output_print(const A& container, const double &timeDifferen
 // };
 
 template <typename A, typename B>
-PmergeMe<A, B>::PmergeMe(const int argc, char **argv): _straggler(-1)
+PmergeMe<A, B>::PmergeMe(const int &argc, char **argv): _straggler(-1)
 {
 	/**
 	 * @brief prints before and after, before time is started
 	 * 
 	 */
-	{
-		std::string check;
-		const std::string valid = "0123456789";
-		long long int int_check;
-		A numbers;
-		for (int i = 1; i < argc; i++)
-		{
-			check = argv[i];
-			// std::cout << check << std::endl;
-			for (std::string::iterator it = check.begin(); it!= check.end(); it++)
-			{
-				if (valid.find(*it) == std::string::npos)
-					throw PmergeMe<A, B>::invalidInputException();
-			}
-			int_check = atoll(argv[i]);
-			if (int_check > INT_MAX)
-				throw std::invalid_argument("Error: Inputs are more than max int");
-			numbers.push_back(atoi(argv[i]));
-		}
-		if (std::is_sorted(numbers.begin(), numbers.end()))
-			throw PmergeMe<A, B>::isSortedException();
-		std::cout << "Before: ";
-		if (numbers.size() <= 5)
-		{
-			for (size_t i = 0; i < numbers.size(); i++)
-				std::cout << numbers[i] << " ";
-			std::cout << std::endl;
-		}
-		else
-		{
-			for (int i = 0; i < 4;i++)
-				std::cout << numbers[i] << " ";
-			std::cout << "[...]" << std::endl;
-		}
-		std::cout << "After: ";
-		std::sort(numbers.begin(), numbers.end());
-		if (numbers.size() <= 5)
-		{
-			for (size_t i = 0; i < numbers.size(); i++)
-				std::cout << numbers[i] << " ";
-			std::cout << std::endl;
-		}
-		else
-		{
-			for (int i = 0; i < 4;i++)
-				std::cout << numbers[i] << " ";
-			std::cout << "[...]" << std::endl;
-		}
-	}
-	//make jacobsthal seq outside because its not part of sorting
+	pre_print_info(argc, argv);
+	// {
+	// 	std::string check;
+	// 	const std::string valid = "0123456789";
+	// 	long long int int_check;
+	// 	A numbers;
+	// 	for (int i = 1; i < argc; i++)
+	// 	{
+	// 		check = argv[i];
+	// 		// std::cout << check << std::endl;
+	// 		for (std::string::iterator it = check.begin(); it!= check.end(); it++)
+	// 		{
+	// 			if (valid.find(*it) == std::string::npos)
+	// 				throw PmergeMe<A, B>::invalidInputException();
+	// 		}
+	// 		int_check = atoll(argv[i]);
+	// 		if (int_check > INT_MAX)
+	// 			throw std::invalid_argument("Error: Inputs are more than max int");
+	// 		numbers.push_back(atoi(argv[i]));
+	// 	}
+	// 	if (std::is_sorted(numbers.begin(), numbers.end()))
+	// 		throw PmergeMe<A, B>::isSortedException();
+	// 	std::cout << "Before: ";
+	// 	if (numbers.size() <= 5)
+	// 	{
+	// 		for (size_t i = 0; i < numbers.size(); i++)
+	// 			std::cout << numbers[i] << " ";
+	// 		std::cout << std::endl;
+	// 	}
+	// 	else
+	// 	{
+	// 		for (int i = 0; i < 4;i++)
+	// 			std::cout << numbers[i] << " ";
+	// 		std::cout << "[...]" << std::endl;
+	// 	}
+	// 	std::cout << "After: ";
+	// 	std::sort(numbers.begin(), numbers.end());
+	// 	if (numbers.size() <= 5)
+	// 	{
+	// 		for (size_t i = 0; i < numbers.size(); i++)
+	// 			std::cout << numbers[i] << " ";
+	// 		std::cout << std::endl;
+	// 	}
+	// 	else
+	// 	{
+	// 		for (int i = 0; i < 4;i++)
+	// 			std::cout << numbers[i] << " ";
+	// 		std::cout << "[...]" << std::endl;
+	// 	}
+	// }
+	//make jacobsthal seq outside because its not part of sorting or data management
 	std::vector<int> jacobsthal;
 	make_jacobs_sequences((argc / 2), jacobsthal);
 	//translates numbers to indices
 	for (std::vector<int>::iterator it = jacobsthal.begin(); it != jacobsthal.end(); it++)
 		*it = *it -1;
 	double startTime = getProcessorTime();
-
 	// check for only digits in argv, and push all back to int vector
 	std::string check;
 	std::string valid = "0123456789";
@@ -159,7 +149,7 @@ PmergeMe<A, B>::PmergeMe(const int argc, char **argv): _straggler(-1)
 		numbers.pop_back();
 	}
 	/**
-	 * @brief assign vecpairs to first and second
+	 * @brief assign numbers to vecPairs to first and second, pairing the values.
 	 * 
 	 */
 	B vecPairs;
@@ -173,7 +163,6 @@ PmergeMe<A, B>::PmergeMe(const int argc, char **argv): _straggler(-1)
 		std::advance(it_current, 2);
 		std::advance(it_next, 2);
 	}
-	
 	/**
 	 * @brief swap vecpairs to order them
 	 * 
@@ -188,7 +177,7 @@ PmergeMe<A, B>::PmergeMe(const int argc, char **argv): _straggler(-1)
 	}
 	// Sorts vecPairs
 	std::sort(vecPairs.begin(), vecPairs.end(), myComparison);
-	//insert straggler at end
+	//re-insert straggler at end of numbers container
 	//add straggler to vecPairs
 	if (this->_straggler > -1)
 	{
@@ -239,7 +228,13 @@ template <typename A, typename B>
 PmergeMe<A, B>::~PmergeMe()
 {}
 
-//	MEMBER FUNCTIONS
+template <typename A, typename B>
+int PmergeMe<A, B>::getStraggler()const
+{
+	return (this->_straggler);
+}
+
+//Exceptions
 
 template <typename A, typename B>
 const char*	PmergeMe<A, B>::invalidInputException::what() const throw()
@@ -253,11 +248,7 @@ const char*	PmergeMe<A, B>::isSortedException::what() const throw()
 	return ("Error: Already Sorted");
 }
 
-template <typename A, typename B>
-int PmergeMe<A, B>::getStraggler()const
-{
-	return (this->_straggler);
-}
+//	PRIVATE MEMBER FUNCTIONS
 
 template <typename A, typename B>
 double PmergeMe<A, B>::getProcessorTime() const
@@ -266,11 +257,6 @@ double PmergeMe<A, B>::getProcessorTime() const
     double processorTime = static_cast<double>(currentTime) / CLOCKS_PER_SEC * SEC_TO_MS;
     return processorTime;
 }
-
-template <typename A, typename B>
-PmergeMe<A, B>::PmergeMe() :_straggler(-1)
-{}
-
 
 
 template <typename A, typename B>
@@ -293,6 +279,73 @@ int PmergeMe<A, B>::binarySearch(const A &a, const int &item, const int low, con
         return binarySearch(a, item, low, mid - 1);
 }
 
+template <typename A, typename B>
+void PmergeMe<A, B>::output_print(const A& container, const double &timeDifference)const
+{
+	if (typeid(container) == typeid(std::deque<typename A::value_type, typename A::allocator_type>))
+	{
+		std::cout << "Time to process a range of " << container.size()
+			  	  << " elements with std::deque : " 
+			  	  <<  timeDifference << " us" << std::endl;
+	}
+	else if (typeid(container) == typeid(std::vector<typename A::value_type, typename A::allocator_type>))
+	{
+		std::cout << "Time to process a range of " << container.size()
+		  		  << " elements with std::vector : " 
+		  		  <<  timeDifference << " us" << std::endl;
+	}
+}
 
+template <typename A, typename B>
+void	PmergeMe<A, B>::pre_print_info(const int &argc, char **argv)
+{
+	std::string check;
+	const std::string valid = "0123456789";
+	long long int int_check;
+	std::vector<int> numbers;
+	for (int i = 1; i < argc; i++)
+	{
+		check = argv[i];
+		// std::cout << check << std::endl;
+		for (std::string::iterator it = check.begin(); it!= check.end(); it++)
+		{
+			if (valid.find(*it) == std::string::npos)
+				throw PmergeMe<A, B>::invalidInputException();
+		}
+		int_check = atoll(argv[i]);
+		if (int_check > INT_MAX)
+			throw std::invalid_argument("Error: Inputs are more than max int");
+		numbers.push_back(atoi(argv[i]));
+	}
+	if (std::is_sorted(numbers.begin(), numbers.end()))
+		throw PmergeMe<A, B>::isSortedException();
+	std::cout << "Before: ";
+	if (numbers.size() <= 5)
+	{
+		for (size_t i = 0; i < numbers.size(); i++)
+			std::cout << numbers[i] << " ";
+		std::cout << std::endl;
+	}
+	else
+	{
+		for (int i = 0; i < 4;i++)
+			std::cout << numbers[i] << " ";
+		std::cout << "[...]" << std::endl;
+	}
+	std::cout << "After: ";
+	std::sort(numbers.begin(), numbers.end());
+	if (numbers.size() <= 5)
+	{
+		for (size_t i = 0; i < numbers.size(); i++)
+			std::cout << numbers[i] << " ";
+		std::cout << std::endl;
+	}
+	else
+	{
+		for (int i = 0; i < 4;i++)
+			std::cout << numbers[i] << " ";
+		std::cout << "[...]" << std::endl;
+	}
+}
 
 #endif
