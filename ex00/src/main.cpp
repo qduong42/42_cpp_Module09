2a29551234a8fe7711e29403005c035a9c78688b
Huy Duong <qduong@student.42wolfsburg.de>
2023-08-03 21:42:31 +0200

    added verbose error messages when running ./btc without args

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -173,7 +173,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		std::cerr << "Error: Invalid Number of arguments." << std::endl;
+		std::cerr << "Error: Invalid Number of arguments. Use with ./btc [someinputfile.txt]" << std::endl;
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
